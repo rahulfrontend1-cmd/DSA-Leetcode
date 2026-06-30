58e4d486846a0d620c9a4b62769485e9ede2f1b7
rahulfrontend1-cmd <rahul.frontend1@gmail.com>
2026-06-30 08:09:38 +0530

    leetcode solution

---
+++ 217. Contains Duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const set = new Set();
+    for(let i of nums) {
+        if(set.has(i)){
+            return true;
+        }
+        set.add(i);
+    }
+    return false;
+};
---
+++ 242. Valid Anagram.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+    const obj1 = {};
+    const obj2 = {};
+    if (s.length !== t.length) {
+        return false;
+    }
+    for (let i=0; i<s.length; i++) {
+        obj1[s[i]] = (obj1[s[i]] || 0) + 1;
+        obj2[t[i]] = (obj2[t[i]] || 0) + 1;
+    }
+    for(const key in obj1) {
+        if(obj1[key] !== obj2[key]) {
+            return false;
+        }
+    }
+    return true;
+            
+};
